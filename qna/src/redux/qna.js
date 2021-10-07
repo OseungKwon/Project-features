e99f03c0359d5b21e7ff01dcd44f18072cd2747f
OseungKwon <00osos@naver.com>
2021-10-07 22:45:05 +0900

    Update qna

--- qna/src/redux/qna.js
+++ qna/src/redux/qna.js
@@ -9,26 +9,14 @@ const initialState = {
   image: "",
   is_open: "",
   updated_at: "2010년 12월 2일",
-  view_count: "45",
+  view_count: "45"
 };
 export const commentSlice = createSlice({
   name: "qna",
   initialState,
   reducers: {
-    addQna(state, action) {
-      const { content, writer, postId, responseTo, commentId, created_at } =
-        action.payload;
-      state = {
-        content,
-        writer,
-        postId,
-        responseTo,
-        commentId,
-        created_at,
-        exist: true, // 대댓글 있는 댓글 삭제 문제 때문에 임시로 넣어둠
-      };
-    },
-  },
+    addQna(state, action) {}
+  }
 });
 export const { addQna } = commentSlice.actions;
 export default commentSlice.reducer;
