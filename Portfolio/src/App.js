3b498a9b9f03e4566a49794418b52fde01f291a7
OseungKwon <54261116+OseungKwon@users.noreply.github.com>
2021-12-02 16:47:22 +0900

    quote 기능 추가

--- Portfolio/src/App.js
+++ Portfolio/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import Car from "./Component/Car";
 import Carousel from "./Component/Carousel";
 import Projects from "./Component/Projects";
+import Quote from "./Component/Quote";
 import Top from "./Component/Top";
 
 const App = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: "black" }}>
       <Top />
       <Carousel />
       <Carousel />
--- Portfolio/src/Component/Top.js
+++ Portfolio/src/Component/Top.js
@@ -1,16 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import Quote from "./Quote";
+
+const MainStyle = styled.div`
+  padding: 10rem;
+  background-color: black;
+  height: 100rem;
+  color: white;
+
+  grid-template-rows: repeat(4, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+`;
+const QuoteWrapper = styled.div`
+  display: flex;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  div {
+    font-size: 4rem;
+  }
+  div:first-child {
+    padding-right: 40rem;
+  }
+`;
 
 const Top = () => {
   return (
-    <div>
-      <h1>권오승 포트폴리오</h1>
-      <div>
-        <h2>about me</h2>
-      </div>
-      <div>
-        <h2>skills</h2>
-      </div>
-    </div>
+    <MainStyle>
+      <h1>
+        저는
+        <QuoteWrapper>
+          <div>[</div>
+          <div>]</div>
+        </QuoteWrapper>
+        개발자입니다
+        <Quote />
+      </h1>
+    </MainStyle>
   );
 };
 
